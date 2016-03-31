6c3c104db1885285f3f6c17ec03033e44ce663c7
中村一意 <ichii.nakamura@gmail.com>
2016-03-31 22:35:42 +0900

    finish Prime_Numebers

--- python27/Prime_Numbers.py
+++ python27/Prime_Numbers.py
@@ -2,6 +2,16 @@ N = int(raw_input())
 A = []
 for i in range(N):
 	A.append(int(raw_input()))
-print ' '.join(map(str, A))
+
+prime_cnt = 0
+for num in A:
+	divisor_cnt = 0
+	for i in range(1, num+1):
+		if num % i == 0:
+			divisor_cnt += 1
+	if divisor_cnt == 2:
+		prime_cnt += 1
+
+print prime_cnt
 
 
